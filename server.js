48bb01e52c6ded4567413ff998eb709e9cf535f3
Selvakumar <selvakmrk@gmail.com>
2021-09-20 02:49:32 +0530

    server js latest changes added

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/kanban-board-angular'));
+app.use(express.static('./dist/assessment-selvakumar'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/kanban-board-angular/'}),
+    res.sendFile('index.html', {root: 'dist/assessment-selvakumar/'}),
 );
 
 // Start the app by listening on the default Heroku port
